60497b38b2ad158833973fbbfd4688f64c2ff954
Mhar <mhar0987@live.com>
2018-02-13 17:14:25 +0800

    ^^

--- application/views/frontend/pages/email.php
+++ application/views/frontend/pages/email.php
@@ -7,7 +7,7 @@
 
 	<p>Hello <?php echo $name; ?>, </p>
 
-	<p>Congrats on signing up for Zoom! In order to activate your account please click the button below to verify your email address: </p>
+	<p>Congrats on signing up for Gravybaby! In order to activate your account please click the button below to verify your email address: </p>
 	<br>
 	<a href="<?php echo site_url("login/code/".$activation_code); ?>" style="padding: 20px;margin: 30px 0px;background-color: #16A085;color:white;border-radius: 10px;text-decoration:none; ">ACTIVATE ACCOUNT</a>
 	<br>
