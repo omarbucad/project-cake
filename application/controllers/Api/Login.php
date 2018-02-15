72486187870df36f4ac7987cbf67918d93c9b09b
Mhar <mhar0987@live.com>
2018-02-15 16:46:45 +0800

    ^^

--- application/controllers/Api/Login.php
+++ application/controllers/Api/Login.php
@@ -5,20 +5,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
 * 
 */
-class Login extends CI_Controller {
+class Login extends API_Controller {
 
-	function __construct( ) {
+	private $post;
+
+	public function __construct( ) {
 		parent::__construct();
 
+		//http://stackoverflow.com/questions/18382740/cors-not-working-php
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+
+		$this->post = file_get_contents("php://input");
 	}
 	
 	public function login () {
-		if($_POST) {
+
+		if($this->post) {
 			$result = $this->db->select("*")->from('users')->get()->result();
+
 			foreach ($result as $key => $value) {
 				$result[$key]->image = site_url("thumbs/images/user/".$value->image_path."/50/50/".$value->image_name);
 			}
-			return $result;
+			
+			echo json_encode($result);
+
 		}
 	}
 
